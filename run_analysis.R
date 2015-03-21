a9825de1cb06b6fb69ab79d97af73c77c668546c
ramsubr2 <vprameshb@gmail.com>
2015-03-21 11:12:43 -0400

    second commit with project completed but not finalized

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,19 @@
 library(data.table)
 library(dplyr)
 
-# Data files are in current working directory
+# All Data files are in current working directory
+# Make sure you have installed the "data.table" and "dplyr" packages
 
-####Merges the training and the test sets to create one data set.
+#### 1. Merges the training and the test sets to create one data set.
 
 #Read the training and text data files and merge them to create one dataset
+print("Reading Training and Test data sets and Merging...")
 train_data_tbl <- read.table("./X_train.txt")
 test_data_tbl <- read.table("./X_test.txt")
-merged_data_tbl <- merge(train_data_tbl, test_data_tbl, all = TRUE)
+merged_data_tbl <- rbind(train_data_tbl, test_data_tbl)
 
-###Extracts only the measurements on the mean and standard deviation for each measurement. 
-
+### 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+print("Extracting only mean and standard deviation measurements...")
 #Read the features.txt file to get Variable names for the columns in the merged data set
 features_tbl <- read.table("./features.txt")
 
@@ -23,16 +25,17 @@ for (i in 1:numVariables) {
 }
 colnames(merged_data_tbl) <- colNames
 
-#eliminating duplicate column names - 84 columns should be eliminated as duplicates
+#eliminating duplicate column names
 merged_data_tbl <- merged_data_tbl[,!duplicated(colnames(merged_data_tbl))]
 #convert to dplyr table
 merged_data_tbl_p <- tbl_df(merged_data_tbl)
+rm("merged_data_tbl") #since we don't need the original merged data table anymore
 
 #extract only mean and std from the measurements - 86 columns have mean and std measurements
 mean_std_only_tbl <- select(merged_data_tbl_p, contains("-mean"), contains("-std"), contains("Mean"))
 
-###Uses descriptive activity names to name the activities in the data set
-
+### 3. Uses descriptive activity names to name the activities in the data set
+print("Adding descriptive activity names to the merged data set...")
 #Read the training and test subject files and merge them into one
 train_subject_tbl <- read.table("./subject_train.txt")
 test_subject_tbl <- read.table("./subject_test.txt")
@@ -49,9 +52,22 @@ activities_tbl[,2] = gsub("_", " ", tolower(as.character(activities_tbl[,2])))
 names(merged_activity_tbl) <- "Activity"
 merged_activity_tbl[,1] = activities_tbl[merged_activity_tbl[,1],2]
 
-###Appropriately labels the data set with descriptive variable names. 
+### 4. Appropriately labels the data set with descriptive variable names. 
 names(merged_subject_tbl) <- "Subject"
-tidy_tbl = cbind(merged_subject_tbl, merged_activity_tbl,mean_std_only_tbl)
+tidy_tbl = cbind(merged_subject_tbl,merged_activity_tbl,mean_std_only_tbl)
+tidy_tbl_p <- tbl_df(tidy_tbl)
+rm("tidy_tbl") # since we don't need tidy table anymore
 
 # write the cleaned up data set
-write.table(tidy_table, "mean_std_only_tidy.txt")
+print("Writing cleaned up data set with mean and std measurements...")
+write.table(tidy_tbl_p, "mean_std_only_tidy.txt")
+
+### 5. creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+print("Creating a second tidy data set with averages for each activity and subject...")
+tidy_tbl_by_subject_activity <- group_by(tidy_tbl_p, Subject, Activity)
+tdy_tbl_avg_each_variable <- summarise_each(tidy_tbl_by_subject_activity,funs(mean))
+
+#write this data set
+print("Writing the data set 'avg_grouped_by_subject_activity.txt'...")
+write.table(tdy_tbl_avg_each_variable, "avg_grouped_by_subject_activity.txt")
+
